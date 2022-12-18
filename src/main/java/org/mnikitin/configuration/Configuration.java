2cca8ded04720d62c7bce1c81cb78a3630f016bb
Maxim Nikitin <nmax_2002@mail.ru>
2022-12-18 22:36:40 +0300

    Исправил найденные ошибки

--- src/main/java/org/mnikitin/configuration/Configuration.java
+++ src/main/java/org/mnikitin/configuration/Configuration.java
@@ -1,24 +1,23 @@
 package org.mnikitin.configuration;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.io.InputStreamReader;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.Properties;
 
 public class Configuration {
-    private static final String FILE_NAME = "server.properties";
+    private static final String FILE_NAME = "/server.properties";
     private static final String DEFAULT_PORT = "12345";
     private final int port;
 
     private static final Configuration c = new Configuration();
 
     private Configuration() {
-        var rootPath = Objects
-                .requireNonNull(Thread.currentThread().getContextClassLoader().getResource(""))
-                .getPath();
-        try (var reader = Files.newBufferedReader(Paths.get(rootPath + FILE_NAME))) {
+        try (var in = Objects.requireNonNull(getClass().getResourceAsStream(FILE_NAME));
+             var reader = new BufferedReader(new InputStreamReader(in))
+        ) {
             var props = new Properties();
             props.load(reader);
             port = Integer.parseInt(Optional.ofNullable(props.getProperty("port")).orElse(DEFAULT_PORT));
